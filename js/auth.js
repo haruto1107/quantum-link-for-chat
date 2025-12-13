35c75b6fc0e7400b7a43e8d9e118f0b435438204
quantum-link <hk62603102@gmail.com>
2025-12-13 09:50:28 +0900

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,31 @@
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// 新規登録
+window.signup = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    location.href = "profile.html";
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+// ログイン
+window.login = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    location.href = "chat.html";
+  } catch (e) {
+    alert(e.message);
+  }
+};
